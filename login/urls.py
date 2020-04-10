6a2fdfe082fb08fd649c6b9a2051412f5a8b0be0
piyush_sinha <piyushsinha19807@gmail.com>
2020-04-10 17:31:13 +0530

    frontend login page

---
+++ login/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from.import views
+
+
+urlpatterns=[
+    path('',views.indexView,name='name'),
+    path('dashboard/',views.dashboardView,name='dashboard'),
+    path('login/',views.loginPage,name='login_url'),
+
+    path('register/',views.registerView,name="register_url"),
+    path('logout/',views.logoutUser,name='logout'),
+
+]
